f9810963ccb22326926ba8d5ab4bb245f939aa2f
Nick Romano <nromano@utk.edu>
2020-08-06 15:56:35 -0400

    Updated utk data

--- initUtkCharts.js
+++ initUtkCharts.js
@@ -1,11 +1,11 @@
 // manually updated from https://veoci.com/veoci/p/form/4jmds5x4jj4j#tab=entryForm
 // eventually should be automatic!!
 const utkCovidCases ={
-  "students": 17,
+  "students": 16,
   "faculty":3,
-  "staff": 7,
-  "cumulativeSinceJune8": 103,
-  "updated": "8/3/20 15:22"
+  "staff": 5,
+  "cumulativeSinceJune8": 107,
+  "updated": "8/6/20 11:31"
 }
 
 document.getElementById('utk-stats-updated').textContent = utkCovidCases.updated;
